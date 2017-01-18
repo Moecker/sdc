099dec842150b6409c49716bd8d424b8a967feb4
Sascha <moeckersascha@hotmail.com>
2017-01-18 21:52:49 +0100

    Added Exploration images

--- histogram_test.py
+++ histogram_test.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 
 from model import kFolderName, kAddingFactor, add_curvy_images
+from keras.models import model_from_json
+from keras.utils.visualize_util import plot
 
 def main():
     # Read the whole csv file containing the udacity input data
@@ -13,7 +15,13 @@ def main():
     input_data = add_curvy_images(input_data)
     print("Equalized size of input data: " + str(len(input_data)))
     
+def plot_model():
+    with open('model.json', 'r') as jfile:
+        model = model_from_json(jfile.read())
+
+    model.compile("adam", "mse")
+    plot(model, to_file='model.png', show_shapes=True)
+    
 if __name__ == "__main__":
-    # Call the main routine
-    main()
+    plot_model()
     
